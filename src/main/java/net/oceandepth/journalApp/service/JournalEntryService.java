b0692797b577dc456e12885539a15f4b6c5fc29b
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-23 22:33:26 +0500

    changed time and date formate from controller to service class

--- src/main/java/net/oceandepth/journalApp/service/JournalEntryService.java
+++ src/main/java/net/oceandepth/journalApp/service/JournalEntryService.java
@@ -1,22 +1,30 @@
 package net.oceandepth.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.oceandepth.journalApp.entity.JournalEntry;
 import net.oceandepth.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
     public void saveEntry(JournalEntry journalEntry) {
-        journalEntryRepository.save(journalEntry);
+
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        } catch(Exception e) {
+            log.error("Exception", e);
+        }
     }
 
     public List<JournalEntry> getAll() {
