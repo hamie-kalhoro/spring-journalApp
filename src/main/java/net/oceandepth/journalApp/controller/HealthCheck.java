5db0ff2d8eb9664ac9fbe42b3afc1216d8baf442
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-24 11:41:47 +0500

    modified health check class

--- src/main/java/net/oceandepth/journalApp/controller/HealthCheck.java
+++ src/main/java/net/oceandepth/journalApp/controller/HealthCheck.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("health")
 public class HealthCheck {
 
-    @GetMapping("health-check")
+    @GetMapping("check")
     public String check() {
         return "ok!";
     }
