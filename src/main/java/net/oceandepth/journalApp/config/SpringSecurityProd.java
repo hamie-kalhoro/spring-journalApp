eb70879a5984a01b1f9ef7641825c38729025f28
hamie-kalhoro <aleehamid1024@gmail.com>
2024-10-04 01:52:05 +0500

    changing profiles using bean

--- src/main/java/net/oceandepth/journalApp/config/SpringSecurityProd.java
+++ src/main/java/net/oceandepth/journalApp/config/SpringSecurityProd.java
@@ -4,6 +4,7 @@ import net.oceandepth.journalApp.service.UserDetailsServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,20 +16,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurity {
+@Profile("prod")
+public class SpringSecurityProd {
 
     UserDetailsServiceImp userDetailsServiceImp;
     @Autowired
-    public SpringSecurity(UserDetailsServiceImp userDetailsServiceImp) {
+    public SpringSecurityProd(UserDetailsServiceImp userDetailsServiceImp) {
         this.userDetailsServiceImp = userDetailsServiceImp;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(request -> request
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/journal/**", "/user/**").authenticated()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
