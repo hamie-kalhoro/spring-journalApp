b98c4defccb3b3320ddd16b96eed5c1c205bb087
hamie-kalhoro <aleehamid1024@gmail.com>
2024-10-07 16:54:43 +0500

    cleaning and packaging the jar file of aplication along with replacing component annotation with service annotation

--- src/main/java/net/oceandepth/journalApp/service/WeatherService.java
+++ src/main/java/net/oceandepth/journalApp/service/WeatherService.java
@@ -4,10 +4,10 @@ import net.oceandepth.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
     private static final String apiKey = "6d16fd7350892af4daf0104c2138c7f8";
